e8fbac82fb17d3fb05bb8e75783cb6bf62503f49
avamel <xnuamx@gmail.com>
2013-07-25 18:51:26 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pressbal::Application.config.secret_key_base = '6772edc7475d761a20f9a7d996007e12f89af8a292a67100bd3a2f092e2653e6403f6a700e82f853ca001e5cafcc842bec28d185e901b33b60a7cb51926b46b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pressbal::Application.config.session_store :cookie_store, key: '_pressbal_session'
