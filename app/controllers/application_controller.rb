044a6ab73ca506b924ab500fb736f292896338b6
avamel <xnuamx@gmail.com>
2013-08-08 18:22:23 +0300

    add 500 page and fixing new index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  if Rails.env.production?
     unless Rails.application.config.consider_all_requests_local
       rescue_from Exception, with: :render_500
       rescue_from ActionController::RoutingError, with: :render_404
       rescue_from ActionController::UnknownController, with: :render_404
+      rescue_from AbstractController::ActionNotFound, with: :render_404
       rescue_from ActiveRecord::RecordNotFound, with: :render_404
-    end
   end
+
 
   def set_info
     if Info.last.present?
@@ -21,5 +21,12 @@ class ApplicationController < ActionController::Base
       @address = Info.last.address
       @address_link = Info.last.address_link
     end
+  end
+  private
+  def render_404
+    render :template => "/public/404.html", :status => 404
+  end
+  def render_500
+   render :template => "/public/500.html", :status => 500
   end
 end
